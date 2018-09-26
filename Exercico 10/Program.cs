3062de0934b29595990dab5cc3bac4d6d3ac004e
BuenoBueno <viniciusbueno2002@gmail.com>
2018-09-26 17:09:19 -0300

    v1.0
    While
    Lista 3
    Exercicio 10

---
+++ Exercico 10/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercico_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Quantidade de Pares e a Soma de Impares");
+            Console.WriteLine();
+
+            int notas = 0;
+            double totalNotas = 0;
+            int i = 1;
+
+            Console.Write("informe a quantidade de alunos na sala: ");
+            int totalAlunos = Convert.ToInt32(Console.ReadLine());
+            while (i <= totalAlunos)
+            {
+
+                Console.Write("Digite a nota do " + i + "° aluno: ");
+                notas = Convert.ToInt32(Console.ReadLine());
+
+                totalNotas = totalNotas + notas;
+
+                i++;
+            }
+
+            double media =  totalNotas/ totalAlunos;
+            Console.WriteLine();
+
+            Console.WriteLine("A média das notas dessa sala é: " + media);
+
+            Console.ReadKey();
+        }
+    }
+}
